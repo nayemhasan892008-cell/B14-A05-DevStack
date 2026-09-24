8b2d77ee3aa9057d5bbdf93706582c1542683d07
nayemhasan892008 <nayemhasan892008@gmail.com>
2026-09-24 19:56:44 +0600

    Initialize Dev Stack project

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+function App() {
+  
+
+  return (
+    <>
+      <div className=" text-4xl  text-red-300">app .tsss</div>
+      <button className="btn btn-secondary   ">Secondary</button>
+      
+      src/
+├── components/
+│   ├── Navbar.tsx
+│   ├── Hero.tsx
+│   ├── TechnologyCard.tsx
+│   ├── TechnologyGrid.tsx
+│   ├── YourStack.tsx
+│   └── Footer.tsx
+│
+├── data/
+│   └── technologies.json
+│
+├── types/
+│   └── technology.ts
+
+      
+
+
+    </>
+  );
+}
+
+export default App;
