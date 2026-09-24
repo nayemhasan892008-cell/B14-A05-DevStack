29b603987a56321b880f90d530586abe2553864e
nayemhasan892008 <nayemhasan892008@gmail.com>
2026-09-25 01:11:46 +0600

    Add navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav className='py-7 border-b border-gray-200'>
+
+      <div className='flex justify-between items-center container mx-auto '>
+
+   <div className='flex justify-between items-center gap-3'>
+    <div className='bg-linear-to-tr from-[#EC4899] to-[#7C3AED]  rounded-lg font-bold text-[#FFFFFF] text-sm flex  items-center justify-center w-10 h-10'>DS</div>
+    <div className='flex items-center justify-center gap-0.5'>
+      <h1 className='font-bold text-xl text-[#0F172A]'>Dev</h1>
+      <h1 className='font-bold text-xl text-[#DB2777]'>Stack</h1>
+    </div>
+   </div>
+
+  
+      <ul className='flex justify-between items-center gap-7 '>
+        <li className='text-[#DB2777] font-semibold text-sm'>Home</li>
+        <li className='text-[#475569] font-medium text-sm'>Technology</li>
+        <li className='text-[#475569] font-medium text-sm'>Projects</li>
+        <li className='text-[#475569] font-medium text-sm'>About</li>
+        <li className='text-[#475569] font-medium text-sm'>Contact</li>
+      </ul>
+      
+    <div className='flex items-center gap-5'>
+      <button className='text-[#334155] font-medium text-sm'>Sign In</button>
+      <button className='font-semibold text-sm text-[#FFFFFF]  rounded-full  bg-[#D91B7E] py-2 px-5'>Sign Up</button>
+    </div>
+    </div>
+    </nav>
+      
+  
+  );
+};
+
+export default Navbar;
